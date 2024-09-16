8fabc98a192dcfca2488ddcaf35810648d2b953f
Farid Alijani <farid.alijani@gmail.com>
2024-09-16 19:02:25 +0300

    check first commit with puhti

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torchvision.models as models
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
+        resnet = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         # disable learning for parameters
         for param in resnet.parameters():
             param.requires_grad_(False)
