e6b9329c23e9af6f2b2f14a7f024cf1070d6fe2e
Farid Alijani <farid.alijani@gmail.com>
2024-09-16 22:29:14 +0300

    sbatch 88g gpu

--- puhti_sbatch_img_captioning.sh
+++ puhti_sbatch_img_captioning.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=188G
+#SBATCH --mem=88G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --gres=gpu:v100:1
