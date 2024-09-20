2a90d6eaa059616fabd73a0550766b7ff3397069
Farid Alijani <farid.alijani@gmail.com>
2024-09-20 22:47:39 +0300

    train 25 epochs added to names

--- puhti_sbatch_img_captioning.sh
+++ puhti_sbatch_img_captioning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2004072
-#SBATCH --job-name=img_captioning_pytorch
+#SBATCH --job-name=img_captioning_pytorch_25epochs
 #SBATCH --output=/scratch/project_2004072/IMG_Captioning/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
