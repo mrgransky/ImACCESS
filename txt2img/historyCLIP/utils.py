00c2c18f2178c2f328bfe202fb5100d0267c997a
Farid Alijani <farid.alijani@gmail.com>
2024-11-04 18:27:08 +0200

    check clean text for numbers too

--- txt2img/historyCLIP/utils.py
+++ txt2img/historyCLIP/utils.py
@@ -217,10 +217,15 @@ def set_seeds():
 		torch.backends.cudnn.deterministic = True
 		torch.backends.cudnn.benchmark = True
 
+# def clean_(text: str="sample text"):
+# 	text = re.sub(r'[";=&#<>_\-\+\^\.\$\[\]]', '', text.lower())
+# 	text = re.sub(r'[^\w\s]', '', text) # Remove punctuation
+# 	text = re.sub(r'[^a-zA-Z0-9\s]', '', text) # Remove special characters
+# 	text = re.sub(r'\s+', ' ', text).strip() # Normalize whitespace
+# 	return text
+
 def clean_(text: str="sample text"):
-	text = re.sub(r'[";=&#<>_\-\+\^\.\$\[\]]', '', text.lower())
-	text = re.sub(r'[^\w\s]', '', text) # Remove punctuation
-	text = re.sub(r'[^a-zA-Z0-9\s]', '', text) # Remove special characters
+	text = re.sub(r'[^a-zA-Z\s]', '', text.lower())
 	text = re.sub(r'\s+', ' ', text).strip() # Normalize whitespace
 	return text
 
