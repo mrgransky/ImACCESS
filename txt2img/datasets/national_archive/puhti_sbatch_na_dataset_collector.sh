2e3ade13831cf21f7d711b32f0663c54e7b22776
Farid Alijani <farid.alijani@gmail.com>
2024-11-27 12:32:04 +0200

    check pouta

--- txt2img/datasets/national_archive/puhti_sbatch_na_dataset_collector.sh
+++ txt2img/datasets/national_archive/puhti_sbatch_na_dataset_collector.sh
@@ -36,7 +36,7 @@ python -u data_collector.py \
 	--dataset_dir $ddir \
 	--start_date  $st_dt \
 	--end_date  $end_dt \
-	--num_worker 39 \
+	--num_worker 10 \
 	--img_mean_std True
 
 done_txt="$user finished Slurm job: `date`"
