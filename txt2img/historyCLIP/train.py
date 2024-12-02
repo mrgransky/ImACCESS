ed55596a5f88310f2ad9c28d32967ff9bb82d360
Farid Alijani <farid.alijani@gmail.com>
2024-12-02 20:36:27 +0200

    check pouta batch_size 64

--- txt2img/historyCLIP/train.py
+++ txt2img/historyCLIP/train.py
@@ -17,7 +17,7 @@ from dataset_loader import HistoricalDataset
 # $ nohup python -u train.py -ddir /media/volume/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1935-01-01_1950-12-31 -vddir /media/volume/ImACCESS/WW_DATASETs/EUROPEANA_1900-01-01_1970-12-31 -nep 50 --device "cuda:0" -lr 1e-3 -wd 5e-2 -ps 5 -is 160 -bs 62 > /media/volume/trash/ImACCESS/historyCLIP_train_NA_val_EUROPEANA_cuda0.out &
 
 # with splited dataset of NA:
-# $ nohup python -u train.py -ddir /media/volume/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1935-01-01_1950-12-31 -nep 30 --device "cuda:2" -lr 1e-3 -wd 5e-2 -ps 5 -is 160 -bs 63 -nw 20 -vds 0.25 > /media/volume/trash/ImACCESS/historyCLIP_train_NA_val_NA_cuda2.out &
+# $ nohup python -u train.py -ddir /media/volume/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1935-01-01_1950-12-31 -nep 30 --device "cuda:2" -lr 1e-3 -wd 5e-2 -ps 5 -is 160 -bs 64 -nw 20 -vds 0.25 > /media/volume/trash/ImACCESS/historyCLIP_train_NA_val_NA_cuda2.out &
 
 # Puhti:
 # $ python train.py -ddir /scratch/project_2004072/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1913-01-01_1946-12-31
