b576b9201bf8a4179871388b58cf112723328155
Farid Alijani <farid.alijani@gmail.com>
2024-12-16 20:28:32 +0200

    printing edited pouta

--- clip/finetune.py
+++ clip/finetune.py
@@ -193,12 +193,6 @@ def finetune(model, train_loader, test_loader, num_epochs=5):
 			print(f"{name} found! => Freezing...")
 			param.requires_grad = False
 
-	# Freeze the early layers in the text encoder
-	for name, param in model.text.named_parameters():
-		if name in ['embedding.weight', 'encoder.layers[0].weight', 'encoder.layers[0].bias', 'encoder.layers[1].weight', 'encoder.layers[1].bias', 'encoder.layers[2].weight', 'encoder.layers[2].bias', 'encoder.layers[3].weight', 'encoder.layers[3].bias']:
-			print(f"{name} found! => Freezing...")
-			param.requires_grad = False
-
 	# Update the remaining layers
 	for param in model.parameters():
 		if param.requires_grad == False:
