c3b9de916ba3a847dd39f851a1a01d73fb5eda2c
Farid Alijani <farid.alijani@gmail.com>
2024-12-17 13:45:10 +0200

    preK cifar10x name adjusted

--- clip/puhti_sbatch_clip_evaluate.sh
+++ clip/puhti_sbatch_clip_evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=prec_at_k_
+#SBATCH --job-name=prec_at_k_CIFAR10x_
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
