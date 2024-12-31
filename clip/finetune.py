eebfc3511c30eab480d7240e6100a37f6481015e
Farid Alijani <farid.alijani@gmail.com>
2024-12-31 20:22:20 +0200

    128 epochs for cifar100 check

--- clip/finetune.py
+++ clip/finetune.py
@@ -9,7 +9,7 @@ from datasets import *
 # $ nohup python -u finetune.py -d cifar100 -bs 260 -ne 32 -lr 5e-6 -wd 1e-3 --print_every 100 -nw 25 --device "cuda:1" -md "ViT-B/32" > /media/volume/ImACCESS/trash/cifar100_train.out &
 
 # strategic finetune cifar100:
-# $ nohup python -u finetune.py -d cifar100 -bs 512 -ne 32 -lr 1e-5 -wd 1e-2 --print_every 100 -nw 50 --device "cuda:1" -md "ViT-B/32" > /media/volume/ImACCESS/trash/cifar100_sft.out &
+# $ nohup python -u finetune.py -d cifar100 -bs 260 -ne 128 -lr 5e-4 -wd 1e-3 --print_every 50 -nw 50 --device "cuda:1" -md "ViT-B/32" > /media/volume/ImACCESS/trash/cifar100_sft.out &
 
 # finetune CINIC10 dataset with given frozen layers:
 # $ nohup python -u finetune.py -d cinic10 -bs 256 -ne 32 -lr 1e-5 -wd 1e-3 --print_every 100 -nw 50 --device "cuda:0" -md "ViT-B/32" -fl visual.conv1 visual.ln_pre > /media/volume/ImACCESS/trash/cinic10_finetune.out &
