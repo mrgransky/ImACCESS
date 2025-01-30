479872b3491c785aac7db5688821935062550af5
Farid Alijani <farid.alijani@gmail.com>
2025-01-30 13:01:16 +0200

    mem and cpu/task increase

--- txt2img/historyCLIP/puhti_sbatch_evaluate.sh
+++ txt2img/historyCLIP/puhti_sbatch_evaluate.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=24G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --array=0-3
