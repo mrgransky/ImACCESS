0768a5552502e0c8f0dcd7f1244088911565be3b
Farid Alijani <farid.alijani@gmail.com>
2025-02-17 11:36:39 +0200

    eval for 15 10 5 1 name adjust

--- txt2img/historyCLIP/puhti_sbatch_evaluate.sh
+++ txt2img/historyCLIP/puhti_sbatch_evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=reverse_preck_dataset_x
+#SBATCH --job-name=reverse_trial_x2_preck_dataset_x
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
