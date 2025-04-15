edc977855bcc6a631fddbd72d2a9da26ce7c678f
Farid Alijani <farid.alijani@gmail.com>
2025-04-15 11:00:45 +0300

    10 cpus

--- txt2img/historyCLIP/puhti_sbatch_finetuner.sh
+++ txt2img/historyCLIP/puhti_sbatch_finetuner.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=80G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
