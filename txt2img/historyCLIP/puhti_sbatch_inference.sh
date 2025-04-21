d5ea1d013a7840f19315eda8b84b3f759ec96884
Farid Alijani <farid.alijani@gmail.com>
2025-04-21 15:29:05 +0300

    all checkpoints in one debug 20cpus

--- txt2img/historyCLIP/puhti_sbatch_inference.sh
+++ txt2img/historyCLIP/puhti_sbatch_inference.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=20G
 #SBATCH --partition=gpu
 #SBATCH --time=01-00:00:00
 #SBATCH --array=5,10,15 # History_X4
