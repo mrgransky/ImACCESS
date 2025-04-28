6ec53484a1091a64131651c97d2ffd933ab5c974
Farid Alijani <farid.alijani@gmail.com>
2025-04-28 17:19:19 +0300

    title y offset adjusted

--- datasets/misc/visualize.py
+++ datasets/misc/visualize.py
@@ -454,7 +454,7 @@ def create_distribution_plot_with_long_tail_analysis(
 	ymax = label_counts.max() * 1.1  # Set maximum y-value for shading
 	
 	segment_opacity = 0.2
-	segment_text_yoffset = 1.045 if len(head_labels) < 5 else 1.02
+	segment_text_yoffset = 1.045 if len(head_labels) < 5 else 1.0
 	segment_text_opacity = 0.7
 	if head_indices:
 		ax.axvspan(
