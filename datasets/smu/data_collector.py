13e5058db33b6179153a7d41151dd3ce3bfaa7e7
Farid Alijani <farid.alijani@gmail.com>
2025-06-02 17:45:18 +0300

    normalized path

--- datasets/smu/data_collector.py
+++ datasets/smu/data_collector.py
@@ -28,6 +28,7 @@ parser.add_argument('--img_mean_std', action='store_true', help='calculate image
 parser.add_argument('--val_split_pct', '-vsp', type=float, default=0.35, help='Validation Split Percentage')
 
 args, unknown = parser.parse_known_args()
+args.dataset_dir = os.path.normpath(args.dataset_dir)
 print_args_table(args=args, parser=parser)
 
 SMU_BASE_URL:str = "https://digitalcollections.smu.edu/digital"
