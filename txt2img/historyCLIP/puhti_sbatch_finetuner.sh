8669deb9996a2e8d27152a057f5ec17df23433a6
Farid Alijani <farid.alijani@gmail.com>
2025-06-27 22:42:43 +0300

    proj num 0,4,8

--- txt2img/historyCLIP/puhti_sbatch_finetuner.sh
+++ txt2img/historyCLIP/puhti_sbatch_finetuner.sh
@@ -11,7 +11,7 @@
 #SBATCH --mem=373G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-#SBATCH --array=0-11 # adjust job name!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#SBATCH --array=0,4,8 # adjust job name!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #SBATCH --time=03-00:00:00
 
 set -euo pipefail
