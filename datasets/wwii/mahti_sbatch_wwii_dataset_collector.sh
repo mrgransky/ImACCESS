43197af41bf93797a2cd83e667e5ab435703c345
Farid Alijani <farid.alijani@gmail.com>
2025-08-28 15:15:15 +0300

    dataset collection sbatch

--- datasets/wwii/mahti_sbatch_wwii_dataset_collector.sh
+++ datasets/wwii/mahti_sbatch_wwii_dataset_collector.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=na_dataset_collection
-#SBATCH --output=/lustre/sgn-data/ImACCESS/trash/logs/%x_%a_%N_%j.out
+#SBATCH --account=project_2009043
+#SBATCH --job-name=wwii_dataset_collection
+#SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=9
-#SBATCH --mem=32G
-#SBATCH --partition=normal
-#SBATCH --time=01-00:00:00 # 1 day
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=64G
+#SBATCH --partition=small
+#SBATCH --time=3-00:00:00
 
 user="`whoami`"
 stars=$(printf '%*s' 100 '')
@@ -17,27 +18,23 @@ txt="$user began Slurm job: `date`"
 ch="#"
 echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
 echo "${stars// /*}"
-echo "CPUS/NODE: $SLURM_JOB_CPUS_PER_NODE, MEM/NODE(--mem): $SLURM_MEM_PER_NODE MB"
+echo "CPUS/NODE: $SLURM_JOB_CPUS_PER_NODE, MEM/NODE(--mem): $SLURM_MEM_PER_NODE"
 echo "HOST: $SLURM_SUBMIT_HOST @ $SLURM_JOB_ACCOUNT, CLUSTER: $SLURM_CLUSTER_NAME, Partition: $SLURM_JOB_PARTITION"
 echo "JOBname: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID, WRK_DIR: $SLURM_SUBMIT_DIR"
 echo "nNODES: $SLURM_NNODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
 echo "nTASKS: $SLURM_NTASKS, TASKS/NODE: $SLURM_TASKS_PER_NODE, nPROCS: $SLURM_NPROCS"
-echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK"
-
+echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK, MEM/CPU: $SLURM_MEM_PER_CPU"
+echo "nTASKS/CORE: $SLURM_NTASKS_PER_CORE, nTASKS/NODE: $SLURM_NTASKS_PER_NODE"
+echo "THREADS/CORE: $SLURM_THREADS_PER_CORE"
 echo "${stars// /*}"
-echo "$SLURM_SUBMIT_HOST ($SLURM_CLUSTER_NAME) conda env from Anaconda..."
-
-source activate py39
-
-ddir="/lustre/sgn-data/ImACCESS/WW_DATASETs"
-st_dt="1900-01-01"
-end_dt="1970-12-31"
+echo "$SLURM_SUBMIT_HOST conda env from tykky module..."
+ddir="/scratch/project_2004072/ImACCESS/WW_DATASETs"
+st_dt="1939-09-01"
+end_dt="1945-09-02"
 num_workers=$((SLURM_CPUS_PER_TASK - 1))
 
 python -u data_collector.py \
 	--dataset_dir $ddir \
-	--start_date  $st_dt \
-	--end_date  $end_dt \
 	--batch_size 256 \
 	--num_worker $num_workers \
 	--historgram_bin 60 \
