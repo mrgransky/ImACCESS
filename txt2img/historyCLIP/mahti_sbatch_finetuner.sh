be0c0065f49b71fafba4903b1266f15daf7ae970
Farid Alijani <farid.alijani@gmail.com>
2025-08-29 12:08:11 +0300

    64 cpus with 256 batch size

--- txt2img/historyCLIP/mahti_sbatch_finetuner.sh
+++ txt2img/historyCLIP/mahti_sbatch_finetuner.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=128
+#SBATCH --cpus-per-task=64
 #SBATCH --mem=478G
 #SBATCH --partition=gpusmall
 #SBATCH --gres=gpu:a100:1
