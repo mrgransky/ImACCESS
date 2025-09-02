cbdbde78550a8249f3c059be8ce806dc387b24b3
Farid Alijani <farid.alijani@gmail.com>
2025-09-03 01:12:22 +0300

    cleaner

--- txt2img/historyCLIP/puhti_test.sh
+++ txt2img/historyCLIP/puhti_test.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=project_2014707
 #SBATCH --job-name=test
-#SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/${SLURM_CLUSTER_NAME}_%x_%a_%N_%j_%A.out
+#SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
@@ -15,9 +15,8 @@
 
 set -euo pipefail
 
-user="`whoami`"
 stars=$(printf '%*s' 100 '')
-txt="$user began Slurm job: `date`"
+txt="began Slurm job: `date`"
 ch="#"
 echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
 echo "${stars// /*}"
@@ -27,8 +26,7 @@ echo "JOBname: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID, WRK_DIR: $SLURM_SUBMIT_DIR"
 echo "nNODES: $SLURM_NNODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
 echo "nTASKS: $SLURM_NTASKS, TASKS/NODE: $SLURM_TASKS_PER_NODE, nPROCS: $SLURM_NPROCS"
 echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK"
-echo "$SLURM_SUBMIT_HOST conda virtual env from tykky module..."
 echo "${stars// /*}"
 
-done_txt="$user finished Slurm job: $(date)"
+done_txt="finished Slurm job: $(date)"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
