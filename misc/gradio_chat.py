2be4a81d088803989a3948d2fb3b4526c5cffdde
Farid Alijani <farid.alijani@gmail.com>
2025-09-18 20:25:51 +0300

    device map and torch dtype qwen 3

--- misc/gradio_chat.py
+++ misc/gradio_chat.py
@@ -25,9 +25,9 @@ TOP_K = 3
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 huggingface_hub.login(token=hf_tk)
 
-# model_id = "Qwen/Qwen3-4B-Instruct-2507"
+model_id = "Qwen/Qwen3-4B-Instruct-2507"
 # model_id = "microsoft/Phi-4-mini-instruct"
-model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+# model_id = "mistralai/Mistral-7B-Instruct-v0.3"
 # model_id = "meta-llama/Llama-3.2-3B-Instruct"
 
 cfg = tfs.AutoConfig.from_pretrained(model_id, trust_remote_code=True)
