4cb092d0426ca4620defa77586fde069ebe08e74
Farid Alijani <farid.alijani@gmail.com>
2025-09-18 19:14:25 +0300

    qwen 3 latest

--- misc/gradio_chat.py
+++ misc/gradio_chat.py
@@ -23,10 +23,11 @@ MAX_RETRIES = 3
 EXP_BACKOFF = 2	# seconds ** attempt
 TOP_K = 3
 
-model_id = "Qwen/Qwen3-4B-Instruct-2507"
-# model_id = "meta-llama/Llama-3.2-1B-Instruct"
+# model_id = "Qwen/Qwen3-4B-Instruct-2507"
+model_id = "Qwen/Qwen3-Next-80B-A3B-Instruct"
 # model_id = "microsoft/Phi-4-mini-instruct"
 # model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+# model_id = "meta-llama/Llama-3.2-3B-Instruct"
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 huggingface_hub.login(token=hf_tk)
