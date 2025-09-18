2806fc88176f80df54406edaa9d29d68e73f73b1
Farid Alijani <farid.alijani@gmail.com>
2025-09-18 16:00:21 +0300

    chat llm chek with mistral

--- misc/gradio_chat.py
+++ misc/gradio_chat.py
@@ -7,7 +7,7 @@ EXP_BACKOFF = 2	# seconds ** attempt
 TOP_K = 3
 
 # Load the model and tokenizer
-model_id = "meta-llama/Llama-3.2-3B-Instruct"
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"
 # model_id = "TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 huggingface_hub.login(token=hf_tk)
