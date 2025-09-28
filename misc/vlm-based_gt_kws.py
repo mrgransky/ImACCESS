9770854042e5640b53fff977978464e722c8db7b
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 01:58:52 +0300

    llava 1.5 x3

--- misc/vlm-based_gt_kws.py
+++ misc/vlm-based_gt_kws.py
@@ -9,30 +9,31 @@ model_id = "llava-hf/llava-1.5-7b-hf"
 
 # Load and preprocess image
 url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
-img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+img = Image.open(requests.get(url, stream=True).raw)#.convert('RGB')
 print(f"IMG: {type(img)} {img.size} {img.mode}")
 
 processor = tfs.LlavaNextProcessor.from_pretrained(model_id, use_fast=True, trust_remote_code=True, cache_dir=cache_directory[USER],)
 model = tfs.LlavaNextForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, cache_dir=cache_directory[USER],)
 model.to("cuda:0")
 
-# prepare image and text prompt, using the appropriate prompt template
-url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
-image = Image.open(requests.get(url, stream=True).raw)
-
 # Define a chat history and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
+instruction = 'Describe the image in three keywords.'
+# Prepare text prompt (LLaVA 1.5 uses a different prompt format)
+prompt = f"USER: <image>\n{instruction} ASSISTANT:"
+print(f"PROMPT: {prompt}")
+
 conversation = [
 	{
 		"role": "user",
 		"content": [
-			{"type": "text", "text": "What is shown in this image?"},
+			{"type": "text", "text": instruction},
 			{"type": "image"},
 		],
 	},
 ]
 prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-inputs = processor(images=image, text=prompt, return_tensors="pt").to("cuda:0")
+inputs = processor(images=img, text=prompt, return_tensors="pt").to("cuda:0")
 
 # autoregressively complete prompt
 output = model.generate(**inputs, max_new_tokens=100)
@@ -56,10 +57,6 @@ model = tfs.LlavaForConditionalGeneration.from_pretrained(
 )
 model.to('cuda:0')
 
-# Prepare text prompt (LLaVA 1.5 uses a different prompt format)
-instruction = 'Describe the image in three keywords.'
-prompt = f"USER: <image>\n{instruction} ASSISTANT:"
-print(f"PROMPT: {prompt}")
 # Process inputs
 inputs = processor(
 		text=prompt,
