4bb37e36dfccee64fdd35f6aa6878ba8cd1bc3e0
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 00:10:44 +0300

    error debugging

--- misc/vlm-based_gt_kws.py
+++ misc/vlm-based_gt_kws.py
@@ -6,18 +6,19 @@ import torch
 model_id = "tiiuae/falcon-11B-vlm"
 # model_id = "utter-project/EuroVLM-1.7B-Preview"
 
-processor = LlavaNextProcessor.from_pretrained(model_id, tokenizer_class='PreTrainedTokenizerFast')
-model = LlavaNextForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+processor = LlavaNextProcessor.from_pretrained(model_id, tokenizer_class='PreTrainedTokenizerFast', use_fast=True)
+model = LlavaNextForConditionalGeneration.from_pretrained(model_id, dtype=torch.bfloat16)
 
 url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
 cats_image = Image.open(requests.get(url, stream=True).raw)
-instruction = 'What do you see in this image?'
+print(cats_image.size)
+instruction = 'Describe the image in three words.'
 
-prompt = f"""User:<image>\n{instruction}"""
+prompt = f"User: <image>\n{instruction} Assistant:"
 inputs = processor(prompt, images=cats_image, return_tensors="pt", padding=True).to('cuda:0')
 
 model.to('cuda:0')
-output = model.generate(**inputs, max_new_tokens=4096)
+output = model.generate(**inputs, max_new_tokens=256)
 
 
 prompt_length = inputs['input_ids'].shape[1]
