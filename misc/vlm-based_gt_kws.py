66de733804cee28c3cb7005ef5b8b4651479be72
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 00:44:03 +0300

    larger model

--- misc/vlm-based_gt_kws.py
+++ misc/vlm-based_gt_kws.py
@@ -1,12 +1,9 @@
-from transformers import LlavaForConditionalGeneration, LlavaProcessor
-from PIL import Image
-import requests
-import torch
+from utils import *
 
 # model_id = "tiiuae/falcon-11B-vlm"
 # model_id = "utter-project/EuroVLM-1.7B-Preview"
-model_id = "llava-hf/llava-1.5-7b-hf"
-# model_id = "llava-hf/llava-1.5-13b-hf"
+# model_id = "llava-hf/llava-1.5-7b-hf"
+model_id = "llava-hf/llava-1.5-13b-hf"
 # model_id = "OpenGVLab/InternVL-Chat-V1-2"
 
 # Load and preprocess image
@@ -15,11 +12,12 @@ img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
 print(f"IMG: {type(img)} {img.size} {img.mode}")
 
 # Load the CORRECT processor and model for LLaVA 1.5
-processor = LlavaProcessor.from_pretrained(model_id)
-model = LlavaForConditionalGeneration.from_pretrained(
-    model_id,
-    dtype=torch.float16,
-    low_cpu_mem_usage=True
+processor = tfs.LlavaProcessor.from_pretrained(model_id, use_fast=True, trust_remote_code=True, cache_dir=cache_directory[USER],)
+model = tfs.LlavaForConditionalGeneration.from_pretrained(
+		model_id,
+		dtype=torch.float16,
+		low_cpu_mem_usage=True,
+		cache_dir=cache_directory[USER],
 )
 model.to('cuda:0')
 
@@ -29,13 +27,13 @@ prompt = f"USER: <image>\n{instruction} ASSISTANT:"
 print(f"PROMPT: {prompt}")
 # Process inputs
 inputs = processor(
-    text=prompt,
-    images=img,
-    return_tensors="pt"
+		text=prompt,
+		images=img,
+		return_tensors="pt"
 ).to('cuda:0')
 
 # Generate output
-output = model.generate(**inputs, max_new_tokens=128)
+output = model.generate(**inputs, max_new_tokens=256)
 
 # Decode output
 results = processor.decode(output[0], skip_special_tokens=True).strip()
