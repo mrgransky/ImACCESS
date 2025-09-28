b5355b1c1e436a88cf73b1b78a54e429e7d15ff7
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 01:55:40 +0300

    llava 1.5 x2

--- misc/vlm-based_gt_kws.py
+++ misc/vlm-based_gt_kws.py
@@ -7,15 +7,13 @@ model_id = "llava-hf/llava-1.5-7b-hf"
 # # model_id = "OpenGVLab/InternVL-Chat-V1-2"
 # model_id = "llava-hf/llava-v1.6-mistral-7b-hf"
 
+# Load and preprocess image
+url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
+img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
+print(f"IMG: {type(img)} {img.size} {img.mode}")
 
-from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
-import torch
-from PIL import Image
-import requests
-
-processor = LlavaNextProcessor.from_pretrained(model_id, use_fast=True, trust_remote_code=True, cache_dir=cache_directory[USER],)
-
-model = LlavaNextForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, cache_dir=cache_directory[USER],)
+processor = tfs.LlavaNextProcessor.from_pretrained(model_id, use_fast=True, trust_remote_code=True, cache_dir=cache_directory[USER],)
+model = tfs.LlavaNextForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True, cache_dir=cache_directory[USER],)
 model.to("cuda:0")
 
 # prepare image and text prompt, using the appropriate prompt template
@@ -25,17 +23,15 @@ image = Image.open(requests.get(url, stream=True).raw)
 # Define a chat history and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
 conversation = [
-    {
-
-      "role": "user",
-      "content": [
-          {"type": "text", "text": "What is shown in this image?"},
-          {"type": "image"},
-        ],
-    },
+	{
+		"role": "user",
+		"content": [
+			{"type": "text", "text": "What is shown in this image?"},
+			{"type": "image"},
+		],
+	},
 ]
 prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-
 inputs = processor(images=image, text=prompt, return_tensors="pt").to("cuda:0")
 
 # autoregressively complete prompt
@@ -44,23 +40,19 @@ print("Generated output:")
 print(processor.decode(output[0], skip_special_tokens=True))
 print("="*100)
 
-# Load and preprocess image
-url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
-img = Image.open(requests.get(url, stream=True).raw).convert('RGB')
-print(f"IMG: {type(img)} {img.size} {img.mode}")
 
 # Load the CORRECT processor and model for LLaVA 1.5
 processor = tfs.LlavaProcessor.from_pretrained(
-  model_id, 
-  use_fast=True, 
-  trust_remote_code=True, 
-  cache_dir=cache_directory[USER],
+	model_id, 
+	use_fast=True, 
+	trust_remote_code=True, 
+	cache_dir=cache_directory[USER],
 )
 model = tfs.LlavaForConditionalGeneration.from_pretrained(
-		model_id,
-		torch_dtype=torch.float16,
-		low_cpu_mem_usage=True,
-		cache_dir=cache_directory[USER],
+	model_id,
+	torch_dtype=torch.float16,
+	low_cpu_mem_usage=True,
+	cache_dir=cache_directory[USER],
 )
 model.to('cuda:0')
 
