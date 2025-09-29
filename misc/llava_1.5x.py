d996f5431166a2fd94d06e5d881a99f111c03cb5
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 12:40:01 +0300

    prompt adjusted

--- misc/llava_1.5x.py
+++ misc/llava_1.5x.py
@@ -3,13 +3,10 @@ from utils import *
 # # model_id = "tiiuae/falcon-11B-vlm"
 # # model_id = "utter-project/EuroVLM-1.7B-Preview"
 # model_id = "llava-hf/llava-1.5-7b-hf"
-model_id = "llava-hf/llava-1.5-13b-hf"
+# model_id = "llava-hf/llava-1.5-13b-hf"
 # # model_id = "OpenGVLab/InternVL-Chat-V1-2"
 
-
-
-def process_image(img_path: str, device: str):
-
+def process_image(model_id: str, img_path: str, device: str):
 	try:
 		img = Image.open(img_path)
 	except FileNotFoundError:
@@ -20,43 +17,42 @@ def process_image(img_path: str, device: str):
 		except requests.exceptions.RequestException as e:
 			print(f"ERROR: failed to load image from {img_path} => {e}")
 			return
-  # url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
-  # img = Image.open(requests.get(url, stream=True).raw)
-  print(f"IMG: {type(img)} {img.size} {img.mode}")
+	# url = "https://digitalcollections.smu.edu/digital/api/singleitem/image/stn/989/default.jpg"
+	# img = Image.open(requests.get(url, stream=True).raw)
+	print(f"IMG: {type(img)} {img.size} {img.mode}")
 
-  processor = tfs.LlavaProcessor.from_pretrained(
-    model_id, 
-    use_fast=True, 
-    trust_remote_code=True, 
-    cache_dir=cache_directory[USER],
-  )
-  model = tfs.LlavaForConditionalGeneration.from_pretrained(
-    model_id,
-    torch_dtype=torch.float16,
-    low_cpu_mem_usage=True,
-    cache_dir=cache_directory[USER],
-  )
-  model.to('cuda:0')
+	processor = tfs.LlavaProcessor.from_pretrained(
+		model_id, 
+		use_fast=True, 
+		trust_remote_code=True, 
+		cache_dir=cache_directory[USER],
+	)
+	model = tfs.LlavaForConditionalGeneration.from_pretrained(
+		model_id,
+		torch_dtype=torch.float16,
+		low_cpu_mem_usage=True,
+		cache_dir=cache_directory[USER],
+	)
+	model.to(device)
 
+	instruction = 'Act as a meticulous historical archivist specializing in 20th century documentation. Describe the image in three concrete, factual and literal keywords.'
+	prompt = f"USER: <image>\n{instruction} ASSISTANT:"
+	# print(f"PROMPT: {prompt}")
 
-  instruction = 'Act as a meticulous historical archivist specializing in 20th century documentation. Describe the image in three keywords.'
-  prompt = f"USER: <image>\n{instruction} ASSISTANT:"
-  print(f"PROMPT: {prompt}")
+	# Process inputs
+	inputs = processor(
+		text=prompt,
+		images=img,
+		return_tensors="pt"
+	).to('cuda:0')
 
-  # Process inputs
-  inputs = processor(
-    text=prompt,
-    images=img,
-    return_tensors="pt"
-  ).to('cuda:0')
+	# Generate output
+	output = model.generate(**inputs, max_new_tokens=128)
 
-  # Generate output
-  output = model.generate(**inputs, max_new_tokens=128)
-
-  # Decode output
-  results = processor.decode(output[0], skip_special_tokens=True).strip()
-  print("Generated output:")
-  print(results)
+	# Decode output
+	results = processor.decode(output[0], skip_special_tokens=True).strip()
+	print("Generated output:")
+	print(results)
 
 
 def main():
