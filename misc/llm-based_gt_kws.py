4bddaec9f3986f12cc5e2f897edbac0f976f7a23
Farid Alijani <farid.alijani@gmail.com>
2025-09-29 21:07:33 +0300

    debug label inefficient approach

--- misc/llm-based_gt_kws.py
+++ misc/llm-based_gt_kws.py
@@ -1553,7 +1553,7 @@ def main():
 	else:
 		raise ValueError("Either --csv_file or --description must be provided")
 
-	keywords = get_labels_efficient(
+	keywords = get_labels_inefficient(
 		model_id=args.model_id, 
 		device=args.device, 
 		test_description=descriptions,
