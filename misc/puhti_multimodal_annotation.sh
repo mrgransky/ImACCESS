e45647daa19c28f0861f8e2be7f0cebf613b6a9c
Farid Alijani <farid.alijani@gmail.com>
2025-10-10 21:18:06 +0300

    check qwen vl2.5 3b puhti

--- misc/puhti_multimodal_annotation.sh
+++ misc/puhti_multimodal_annotation.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=373G
+#SBATCH --mem=256G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
 #SBATCH --array=0-4
@@ -45,7 +45,7 @@ python -u multimodal_annotation.py \
   --num_workers $SLURM_CPUS_PER_TASK \
   --batch_size 64 \
   --llm_model_id "Qwen/Qwen3-4B-Instruct-2507" \
-  --vlm_model_id "Qwen/Qwen2.5-VL-7B-Instruct" \
+  --vlm_model_id "Qwen/Qwen2.5-VL-3B-Instruct" \
   --max_generated_tks 64 \
   --max_keywords 5 \
   --verbose
