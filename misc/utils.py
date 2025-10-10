dd51ccc1497dfd6469ab31444902d023fb47b05d
Farid Alijani <farid.alijani@gmail.com>
2025-10-10 15:09:53 +0300

    import sentence transformers

--- misc/utils.py
+++ misc/utils.py
@@ -13,7 +13,7 @@ from sklearn.cluster import KMeans
 from collections import Counter, defaultdict
 # import faiss
 
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer
 from langdetect import detect, DetectorFactory
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.preprocessing import normalize, MultiLabelBinarizer
