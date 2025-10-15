6e3f1dff5a0ce8fdc5359a005ebfa30df15f58bc
Farid Alijani <farid.alijani@gmail.com>
2025-10-15 18:35:52 +0300

    europeana qwen 3 vl

--- misc/multimodal_annotation.py
+++ misc/multimodal_annotation.py
@@ -17,7 +17,7 @@ from visualize import perform_multilabel_eda
 
 # how to run [Pouta]:
 # $ nohup python -u multimodal_annotation.py -csv /media/volume/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1900-01-01_1970-12-31/metadata_multi_label.csv -llm "Qwen/Qwen3-4B-Instruct-2507" -vlm "Qwen/Qwen2.5-VL-3B-Instruct" -bs 64 -dv "cuda:1" -nw 32 -v > /media/volume/ImACCESS/trash/multimodal_annotation_na.txt &
-# $ nohup python -u multimodal_annotation.py -csv /media/volume/ImACCESS/WW_DATASETs/EUROPEANA_1900-01-01_1970-12-31/metadata_multi_label.csv -llm "Qwen/Qwen3-4B-Instruct-2507" -vlm "Qwen/Qwen2.5-VL-3B-Instruct" -bs 64 -dv "cuda:3" -nw 32 -v > /media/volume/ImACCESS/trash/multimodal_annotation_eu.txt &
+# $ nohup python -u multimodal_annotation.py -csv /media/volume/ImACCESS/WW_DATASETs/EUROPEANA_1900-01-01_1970-12-31/metadata_multi_label.csv -llm "Qwen/Qwen3-4B-Instruct-2507" -vlm "Qwen/Qwen3-VL-4B-Instruct" -bs 64 -dv "cuda:3" -nw 32 -v > /media/volume/ImACCESS/trash/multimodal_annotation_eu.txt &
 # $ nohup python -u multimodal_annotation.py -csv /media/volume/ImACCESS/WW_DATASETs/HISTORY_X4/metadata_multi_label.csv -llm "Qwen/Qwen3-4B-Instruct-2507" -vlm "Qwen/Qwen2.5-VL-3B-Instruct" -bs 32 -dv "cuda:2" -nw 32 -v > /media/volume/ImACCESS/trash/multimodal_annotation_h4.txt &
 # $ nohup python -u multimodal_annotation.py -csv /media/volume/ImACCESS/WW_DATASETs/SMU_1900-01-01_1970-12-31/metadata_multi_label.csv -llm "Qwen/Qwen3-4B-Instruct-2507" -vlm "Qwen/Qwen2.5-VL-3B-Instruct" -bs 64 -dv "cuda:3" -v > /media/volume/ImACCESS/trash/multimodal_annotation_smu.txt &
 
