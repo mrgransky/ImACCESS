39d2ebd02ec79abe0d3f1af3e6663583901bafc9
Farid Alijani <farid.alijani@gmail.com>
2025-10-23 01:28:32 +0300

    memory error

--- datasets/national_archive/csc_sbatch_na_dataset_collector.sh
+++ datasets/national_archive/csc_sbatch_na_dataset_collector.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=256G
 #SBATCH --partition=small
 #SBATCH --time=03-00:00:00
 
@@ -31,14 +31,13 @@ echo "$SLURM_SUBMIT_HOST conda env from tykky module..."
 ddir="/scratch/project_2004072/ImACCESS/WW_DATASETs"
 st_dt="1900-01-01"
 end_dt="1970-12-31"
-num_workers=$((SLURM_CPUS_PER_TASK - 1))
 
 python -u data_collector.py \
 	--dataset_dir $ddir \
 	--start_date  $st_dt \
 	--end_date  $end_dt \
-	--batch_size 512 \
-	--num_worker $num_workers \
+	--batch_size 64 \
+	--num_worker $SLURM_CPUS_PER_TASK \
 	--historgram_bin 60 \
 	--img_mean_std \
 	--enable_thumbnailing \
