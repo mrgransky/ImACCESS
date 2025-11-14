a47c0ec793de93839966d12711db52ade686de55
Farid Alijani <farid.alijani@gmail.com>
2025-11-14 16:55:23 +0200

    adapter based add x11

--- historyCLIP/mahti_sbatch_finetuner.sh
+++ historyCLIP/mahti_sbatch_finetuner.sh
@@ -198,9 +198,9 @@ case $strategy in
     ;;
   "probe"|"ia3"|"adapter")
     case $architecture in
-      "ViT-L/14@336px") ADJUSTED_BATCH_SIZE=64 ;;
-      "ViT-L/14")       ADJUSTED_BATCH_SIZE=128 ;;
-      "ViT-B/32"|"ViT-B/16") ADJUSTED_BATCH_SIZE=256 ;;
+      "ViT-L/14@336px") ADJUSTED_BATCH_SIZE=32 ;;
+      "ViT-L/14")       ADJUSTED_BATCH_SIZE=64 ;;
+      "ViT-B/32"|"ViT-B/16") ADJUSTED_BATCH_SIZE=128 ;;
     esac
     ;;
 esac
