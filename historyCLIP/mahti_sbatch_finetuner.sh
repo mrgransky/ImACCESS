264f225ebde190cba80bcd6c279a58598dfefe3a
Farid Alijani <farid.alijani@gmail.com>
2025-11-14 17:47:58 +0200

    every 4th place

--- historyCLIP/mahti_sbatch_finetuner.sh
+++ historyCLIP/mahti_sbatch_finetuner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2014707
-#SBATCH --job-name=h4_multi_label_dataset
+#SBATCH --job-name=h4_multi_label
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
@@ -11,7 +11,7 @@
 #SBATCH --mem=333G
 #SBATCH --partition=gpusmall
 #SBATCH --gres=gpu:a100:1
-#SBATCH --array=0,4,8,12,16,20,24,28,32,36,40
+#SBATCH --array=0-40:4
 #SBATCH --time=1-12:00:00
 
 set -euo pipefail
