7f6d5a2bb4978e18f34a74f79af9c553d5952df9
Farid Alijani <farid.alijani@gmail.com>
2025-11-20 13:53:39 +0200

    10 cps for interactive

--- datasets/national_archive/csc_sbatch_na_dataset_collector.sh
+++ datasets/national_archive/csc_sbatch_na_dataset_collector.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=16G
 #SBATCH --partition=interactive
 #SBATCH --time=01-00:00:00
