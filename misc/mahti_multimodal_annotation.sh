fa8fb6173aa57b0e569054eadf9dfcacbc42e465
Farid Alijani <farid.alijani@gmail.com>
2025-12-01 16:40:30 +0200

    reduce mem

--- misc/mahti_multimodal_annotation.sh
+++ misc/mahti_multimodal_annotation.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=64G
+#SBATCH --mem=48G
 #SBATCH --array=1-4
 #SBATCH --partition=gpusmall
 #SBATCH --time=01-12:00:00
