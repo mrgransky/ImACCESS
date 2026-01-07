5ca48c3f92a2ca297152d0aadafd5524ebb5b0b5
Farid Alijani <farid.alijani@gmail.com>
2026-01-07 14:25:32 +0200

    puhti chunk params updated

--- misc/puhti_chunk_multimodal_annotation.sh
+++ misc/puhti_chunk_multimodal_annotation.sh
@@ -7,11 +7,11 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=48G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=40G
 #SBATCH --partition=gpu
 #SBATCH --time=03-00:00:00
-#SBATCH --array=0-32
+#SBATCH --array=0-21
 #SBATCH --gres=gpu:v100:4,nvme:100
 
 set -euo pipefail
