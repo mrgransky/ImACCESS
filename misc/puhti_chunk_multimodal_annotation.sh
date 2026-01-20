c11837293572c8e10000cbd87452c4e5d884393a
Farid Alijani <farid.alijani@gmail.com>
2026-01-21 01:16:35 +0200

    back 2 gpu small gpu

--- misc/puhti_chunk_multimodal_annotation.sh
+++ misc/puhti_chunk_multimodal_annotation.sh
@@ -7,8 +7,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=6G
 #SBATCH --time=03-00:00:00
 #SBATCH --array=0-24
 #SBATCH --partition=gpu
