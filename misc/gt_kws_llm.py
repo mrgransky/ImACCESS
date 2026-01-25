1e83a6fc1a51a2cb42fd9ef4deb6fac85ed8d017
Farid Alijani <farid.alijani@gmail.com>
2026-01-25 23:12:15 +0200

    with top-K x2

--- misc/gt_kws_llm.py
+++ misc/gt_kws_llm.py
@@ -1522,7 +1522,7 @@ def main():
 	parser.add_argument("--num_workers", '-nw', type=int, default=12, help="Number of workers for parallel processing")
 	parser.add_argument("--batch_size", '-bs', type=int, default=32, help="Batch size for processing (adjust based on GPU memory)")
 	parser.add_argument("--max_generated_tks", '-mgt', type=int, default=256, help="Max number of generated tokens")
-	parser.add_argument("--max_keywords", '-mkw', type=int, default=5, help="Max number of keywords to extract")
+	parser.add_argument("--max_keywords", '-mkw', type=int, default=3, help="Max number of keywords to extract")
 	parser.add_argument("--use_quantization", '-q', action='store_true', help="Use quantization")
 	parser.add_argument("--verbose", '-v', action='store_true', help="Verbose output")
 	parser.add_argument("--debug", '-d', action='store_true', help="Debug mode")
