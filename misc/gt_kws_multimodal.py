f10685a38360c8ae25ffff1a9e0ffb65a7bf1761
Farid Alijani <farid.alijani@gmail.com>
2026-01-28 14:15:54 +0200

    model for clustering

--- misc/gt_kws_multimodal.py
+++ misc/gt_kws_multimodal.py
@@ -231,7 +231,7 @@ def get_multimodal_annotation(
 	print(os.path.join(OUTPUT_DIR, os.path.basename(csv_file).replace(".csv", "_clusters.csv")))
 	_clustering_(
 		labels=multimodal_labels, 
-		model_id="google/embeddinggemma-300M",
+		model_id="google/embeddinggemma-300M" if torch.__version__ > "2.6" else "sentence-transformers/all-MiniLM-L6-v2",
 		nc=nc,
 		clusters_fname=os.path.join(OUTPUT_DIR, os.path.basename(csv_file).replace(".csv", "_clusters.csv"))
 	)
