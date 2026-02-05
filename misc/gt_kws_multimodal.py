9dcf29f5020a65dc5713a50f3dab0f763eb1f020
Farid Alijani <farid.alijani@gmail.com>
2026-02-05 19:58:20 +0200

    using qwen embedding

--- misc/gt_kws_multimodal.py
+++ misc/gt_kws_multimodal.py
@@ -234,7 +234,7 @@ def get_multimodal_annotation(
 
 	cluster(
 		labels=multimodal_labels, 
-		model_id="google/embeddinggemma-300M" if torch.__version__ > "2.6" else "sentence-transformers/all-MiniLM-L6-v2",
+		model_id="Qwen/Qwen3-Embedding-4B",#"google/embeddinggemma-300M" if torch.__version__ > "2.6" else "sentence-transformers/all-MiniLM-L6-v2",
 		nc=nc,
 		clusters_fname=os.path.join(OUTPUT_DIR, os.path.basename(csv_file).replace(".csv", "_clusters.csv")),
 		verbose=verbose,
