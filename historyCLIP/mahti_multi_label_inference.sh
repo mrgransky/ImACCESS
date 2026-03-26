dac109bbe52dc397f701243fe87aebca2c62fa82
Farid Alijani <farid.alijani@gmail.com>
2026-03-26 04:36:02 +0200

    mahti inference

---
+++ historyCLIP/mahti_multi_label_inference.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#SBATCH --account=project_2009043
+#SBATCH --job-name=inference_h4_multi_label
+#SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a.out
+#SBATCH --mail-user=farid.alijani@gmail.com
+#SBATCH --mail-type=END,FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --partition=gpusmall
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=256G
+#SBATCH --time=00-12:00:00
+#SBATCH --begin=09:30:00
+#SBATCH --array=0
+
+set -euo pipefail
+
+user="`whoami`"
+stars=$(printf '%*s' 100 '')
+txt="$user began Slurm job: `date`"
+ch="#"
+echo -e "${txt//?/$ch}\n${txt}\n${txt//?/$ch}"
+echo "${stars// /*}"
+echo "CPUS/NODE: $SLURM_JOB_CPUS_PER_NODE, MEM/NODE(--mem): $SLURM_MEM_PER_NODE"
+echo "HOST: $SLURM_SUBMIT_HOST @ $SLURM_JOB_ACCOUNT, CLUSTER: $SLURM_CLUSTER_NAME, Partition: $SLURM_JOB_PARTITION"
+echo "JOBname: $SLURM_JOB_NAME, ID: $SLURM_JOB_ID, WRK_DIR: $SLURM_SUBMIT_DIR"
+echo "nNODES: $SLURM_NNODES, NODELIST: $SLURM_JOB_NODELIST, NODE_ID: $SLURM_NODEID"
+echo "nTASKS: $SLURM_NTASKS, TASKS/NODE: $SLURM_TASKS_PER_NODE, nPROCS: $SLURM_NPROCS"
+echo "CPUS_ON_NODE: $SLURM_CPUS_ON_NODE, CPUS/TASK: $SLURM_CPUS_PER_TASK"
+echo "$SLURM_SUBMIT_HOST conda virtual env from tykky module..."
+echo "${stars// /*}"
+
+DATASETS=(
+	/scratch/project_2004072/ImACCESS/WW_DATASETs/HISTORY_X4
+	/scratch/project_2004072/ImACCESS/WW_DATASETs/NATIONAL_ARCHIVE_1900-01-01_1970-12-31
+	/scratch/project_2004072/ImACCESS/WW_DATASETs/EUROPEANA_1900-01-01_1970-12-31
+	/scratch/project_2004072/ImACCESS/WW_DATASETs/WWII_1939-09-01_1945-09-02
+	/scratch/project_2004072/ImACCESS/WW_DATASETs/SMU_1900-01-01_1970-12-31
+)
+
+path_files_dir=${DATASETS[$SLURM_ARRAY_TASK_ID]}/multi_label
+
+echo "Processing: ${path_files_dir}"
+
+python -u multi_label_inference.py \
+	--pth_files_directory ${path_files_dir} \
+	--batch_size 16 \
+	--num_workers $SLURM_CPUS_PER_TASK \
+	--model_architecture "ViT-L/14@336px" \
+	--verbose
+
+
+done_txt="$user finished Slurm job: `date`"
+echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
