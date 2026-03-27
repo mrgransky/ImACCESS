8f91d35a63152464786bfffdf31a3e6447c706ad
Farid Alijani <farid.alijani@gmail.com>
2026-03-27 19:47:09 +0200

    diff scaler

--- historyCLIP/puhti_finetuner.sh
+++ historyCLIP/puhti_finetuner.sh
@@ -7,13 +7,13 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=128G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=256G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
-###SBATCH --array=0-40:4
-#SBATCH --array=0
-#SBATCH --time=00-01:00:00
+#SBATCH --array=0-40:4
+###SBATCH --array=0
+#SBATCH --time=03-00:00:00
 
 set -euo pipefail
 
