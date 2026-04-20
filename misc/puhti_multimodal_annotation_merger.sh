75f8de7714b2b90a83e0a2550ce0db26525ced1c
Farid Alijani <farid.alijani@gmail.com>
2026-04-20 16:05:04 +0300

    h4

--- misc/puhti_multimodal_annotation_merger.sh
+++ misc/puhti_multimodal_annotation_merger.sh
@@ -33,7 +33,7 @@ echo "${stars// /*}"
 echo "$SLURM_SUBMIT_HOST conda virtual env from tykky module..."
 echo "${stars// /*}"
 
-DATASET_DIR="/scratch/project_2004072/ImACCESS/WW_DATASETs/_HISTORY_X4"
+DATASET_DIR="/scratch/project_2004072/ImACCESS/WW_DATASETs/HISTORY_X4"
 TEXT_EMBEDDING_MODEL="Qwen/Qwen3-Embedding-8B"
 # TEXT_EMBEDDING_MODEL="nvidia/llama-embed-nemotron-8b"
 # TEXT_EMBEDDING_MODEL="Octen/Octen-Embedding-8B"
