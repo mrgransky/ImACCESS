0d73449eb8c4d6913272d5c24adbf20389351218
Farid Alijani <farid.alijani@gmail.com>
2026-04-22 13:12:38 +0300

    42g mem

--- misc/mahti_multimodal_annotation_merger.sh
+++ misc/mahti_multimodal_annotation_merger.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=48G
+#SBATCH --mem=42G
 #SBATCH --partition=gpusmall
 #SBATCH --time=00-05:00:00
 #SBATCH --gres=gpu:a100:1,nvme:100
