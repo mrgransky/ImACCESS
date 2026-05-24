6e3c1c67df770c41e6875c2d1970abb92d06265c
Farid Alijani <farid.alijani@gmail.com>
2026-05-24 23:31:36 +0300

    pipeline puhti x4

--- Auditable-Supervision-Framework/puhti_pipeline.sh
+++ Auditable-Supervision-Framework/puhti_pipeline.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=18G
+#SBATCH --mem=192G
 #SBATCH --partition=gpu
 #SBATCH --time=00-03:00:00
 #SBATCH --gres=gpu:v100:4,nvme:100
