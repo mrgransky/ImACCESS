68d51e9c8cea17d69bd3035ac47cb0f70a6b50eb
Farid Alijani <farid.alijani@gmail.com>
2026-07-26 18:54:26 +0300

    batch size adjust and eval add

--- Auditable-Supervision-Framework/puhti_pipeline.sh
+++ Auditable-Supervision-Framework/puhti_pipeline.sh
@@ -79,10 +79,13 @@ python -u stage3_4_cgd_consolidation.py -jsonl $JSONL_MODALITY_CONFLICT_FILE -v
 python -u viz.py -jsonl $JSONL_MODALITY_CONFLICT_FILE -v
 
 # stage 5: Regime-Conditioned Training
-TRAINING_BATCH_SIZE=8
+TRAINING_BATCH_SIZE=12
 LEARNING_RATE=1e-4
 CLIP_ARCH="ViT-L/14@336px"
 python -u stage5_run.py -csv $CSV_FILE -cm $CLIP_ARCH -bs $TRAINING_BATCH_SIZE -lr $LEARNING_RATE -v
+
+# evaluation:
+python -u eval_regime_stratified.py -csv $CSV_FILE -cm $CLIP_ARCH -bs $TRAINING_BATCH_SIZE -nw $SLURM_CPUS_PER_TASK -v
 
 done_txt="$user finished Slurm job: `date`"
 echo -e "${done_txt//?/$ch}\n${done_txt}\n${done_txt//?/$ch}"
