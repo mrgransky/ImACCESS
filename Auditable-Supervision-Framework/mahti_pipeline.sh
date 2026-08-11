5aba2f563952417dfa3280a23889057212cf6298
Farid Alijani <farid.alijani@gmail.com>
2026-08-12 00:44:33 +0300

    1 nw

--- Auditable-Supervision-Framework/mahti_pipeline.sh
+++ Auditable-Supervision-Framework/mahti_pipeline.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=108G
 #SBATCH --partition=gpumedium
 #SBATCH --time=01-12:00:00
