d08b6093a021524bd8b02b512f1fee7a7bfa8d86
Farid Alijani <farid.alijani@gmail.com>
2026-08-24 01:22:21 +0300

    finetuner debug x5

--- historyCLIP/mahti_finetuner.sh
+++ historyCLIP/mahti_finetuner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=ft_h4_multi_label_seed_1
+#SBATCH --job-name=ft_seed_2_h4_multi_label
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
