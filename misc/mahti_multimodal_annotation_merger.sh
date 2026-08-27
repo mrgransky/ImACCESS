f9703e28a81f71cc1e4ad513041aaaf80d72a871
Farid Alijani <farid.alijani@gmail.com>
2026-08-27 22:22:07 +0300

    interactive mem increase

--- misc/mahti_multimodal_annotation_merger.sh
+++ misc/mahti_multimodal_annotation_merger.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=24G
+#SBATCH --mem=73G
 #SBATCH --partition=interactive
 #SBATCH --time=00-23:00:00
 ###SBATCH --gres=gpu:a100:1,nvme:150
