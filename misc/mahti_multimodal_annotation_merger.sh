7db1338d1c20b3ef100c2d3be38eb1894f748ab3
Farid Alijani <farid.alijani@gmail.com>
2026-08-27 20:29:07 +0300

    interactive

--- misc/mahti_multimodal_annotation_merger.sh
+++ misc/mahti_multimodal_annotation_merger.sh
@@ -8,10 +8,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=14G
-#SBATCH --partition=gpusmall
+#SBATCH --mem=24G
+#SBATCH --partition=interactive
 #SBATCH --time=00-23:00:00
-#SBATCH --gres=gpu:a100:1,nvme:150
+###SBATCH --gres=gpu:a100:1,nvme:150
 
 set -euo pipefail
 
