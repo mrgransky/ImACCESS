66583f300048d8e66bfffb43158f8dc2de8624f3
Farid Alijani <farid.alijani@gmail.com>
2026-08-29 18:15:35 +0300

    seed 42

--- historyCLIP/csc_finetuner.sh
+++ historyCLIP/csc_finetuner.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=ft_h4_multi_label_seed_2
+#SBATCH --job-name=ft_h4_multi_label_seed_42
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=164G
+#SBATCH --mem=224G
 #SBATCH --partition=gpumedium
 #SBATCH --gres=gpu:gh200:1
 #SBATCH --time=1-12:00:00
@@ -76,7 +76,7 @@
 
 # how to run:
 # ensure the job name aligns with the seed value
-# SEED=2 sbatch csc_finetuner.sh
+# SEED=42 sbatch csc_finetuner.sh
 
 set -euo pipefail
 user="`whoami`"
