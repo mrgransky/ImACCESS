7c8a5192a8f893e55d41281bccf6f2c6cf5be30c
Farid Alijani <farid.alijani@gmail.com>
2026-08-31 18:08:18 +0300

    seed 2 gpu large

--- historyCLIP/csc_finetuner.sh
+++ historyCLIP/csc_finetuner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=ft_h4_multi_label_seed_01
+#SBATCH --job-name=ft_h4_multi_label_seed_02
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a_%N_%j_%A.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
@@ -9,9 +9,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=200G
-#SBATCH --partition=gpumedium
+#SBATCH --partition=gpularge
 #SBATCH --gres=gpu:gh200:2
-#SBATCH --time=1-12:00:00
+#SBATCH --time=0-12:00:00
 ####SBATCH --begin=08:45:00
 ##############################################################################
 # ARRAY INDEXING SCHEME
@@ -77,7 +77,7 @@
 
 # how to run:
 # !!!!! xxxx ensure the job name aligns with the seed value xxxx
-# SEED=1 sbatch csc_finetuner.sh
+# SEED=2 sbatch csc_finetuner.sh
 
 set -euo pipefail
 user="`whoami`"
