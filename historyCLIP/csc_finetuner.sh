2ec8e2cf150b1d83c0723f7e74d7fa30ee27bb96
Farid Alijani <farid.alijani@gmail.com>
2026-09-07 15:44:52 +0300

    hyperparam add for all seed 1

--- historyCLIP/csc_finetuner.sh
+++ historyCLIP/csc_finetuner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #SBATCH --account=project_2009043
-#SBATCH --job-name=ft_seed_02
+#SBATCH --job-name=ft_seed_01
 #SBATCH --output=/scratch/project_2004072/ImACCESS/trash/logs/%x_%a.out
 #SBATCH --mail-user=farid.alijani@gmail.com
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=256G
-#SBATCH --partition=gpularge
-#SBATCH --gres=gpu:gh200:2
+#SBATCH --mem=264G
+#SBATCH --partition=gpumedium
+#SBATCH --gres=gpu:gh200:1
 #SBATCH --time=0-08:00:00
 ####SBATCH --begin=08:45:00
 ##############################################################################
@@ -79,7 +79,7 @@
 
 # how to run:
 # !!!!!!! xxxx ensure the job name aligns with the seed value xxxx
-# SEED=2 sbatch csc_finetuner.sh
+# SEED=1 sbatch csc_finetuner.sh
 
 set -euo pipefail
 user="`whoami`"
