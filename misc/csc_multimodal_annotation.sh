9c83a48cd588348a210e2a3b5c5ce88acd5cc730
Farid Alijani <farid.alijani@gmail.com>
2026-09-16 03:43:07 +0300

    gpu large

--- misc/csc_multimodal_annotation.sh
+++ misc/csc_multimodal_annotation.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=264G
-#SBATCH --partition=gpumedium
+#SBATCH --partition=gpularge
 #SBATCH --gres=gpu:gh200:4
 #SBATCH --time=01-12:00:00
 #SBATCH --array=0
